0c05042738804d583e021f8b5e161bbf171c20ab
nimish <menimish05696@gmail.com>
2019-09-18 10:21:25 +0530

    third commit

--- ps1.py
+++ ps1.py
@@ -4,4 +4,6 @@ for i in range(0, len(data)):
     if data[i] in '01':
         if data[i] in '1':
             count += 1
-
+    else:
+        print("Please enter a valid binary data")
+        break
