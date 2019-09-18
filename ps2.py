3dd141fb9588bf292c8547be00ad7e70af570ff6
nimish <menimish05696@gmail.com>
2019-09-18 14:25:27 +0530

    second commit

---
+++ ps2.py
@@ -0,0 +1,10 @@
+print("Welcome to the game")
+choice=input("Enter your choice of odd or even")
+if choice=="odd":
+    print("You are player 1")
+else:
+    print("You are player 2")
+player="Player 1's chance"
+print(player)
+game(player)
+
